02183a090d44774dc2797f41a5b1cd5069c280d9
Armaan Sengupta <senguptaarmaan@gmail.com>
2025-02-16 17:21:18 -0500

    f_theta6dot calculation, fix use of theta 3 dot instead of theta 3

--- kinematics.m
+++ kinematics.m
@@ -21,7 +21,7 @@ f_3Adot = @(theta2) (f_3A(theta2).*f_theta3dot(theta2).*(sind(f_theta3(theta2)).
             + R2*theta2_velocity.*(sind(theta2).*cosd(f_theta6(theta2)) - cosd(theta2).*sind(f_theta6(theta2)))) ...
             ./ (cosd(f_theta3(theta2)).*cosd(f_theta6(theta2)) + sind(f_theta3(theta2)).*sind(f_theta6(theta2)));
 
-f_theta6dot = @(theta2) (f_3Adot(theta2).*cosd(f_theta3dot(theta2)) - f_3A(theta2).*f_theta3dot(theta2).*sind(f_theta3dot(theta2)) - R2*theta2_velocity.*sind(theta2)) ./ -1*R6.*sind(f_theta6(theta2));
+f_theta6dot = @(theta2) (f_3Adot(theta2).*cosd(f_theta3(theta2)) - f_3A(theta2).*f_theta3dot(theta2).*sind(f_theta3(theta2)) - R2*theta2_velocity.*sind(theta2)) ./ (-R6.*sind(f_theta6(theta2)));
 
 f_ky = @(theta2) (-1.*R6.*(f_theta6dot(theta2).^2).*sind(f_theta6(theta2)) - 2.*f_3Adot(theta2).*f_theta3dot(theta2).*cosd(f_theta3(theta2)) - f_3A(theta2).*f_theta3dotdot(theta2).*cosd(f_theta3(theta2)) + f_3A.*(f_theta3dot(theta2).^2).*sind(f_theta3(theta2)) + R2*(theta2_velocity^2).*sin(theta2));
 f_kx = @(theta2) -1*R6.*(f_theta6dot(theta2).^2).*cosd(f_theta6(theta2)) + 2.*f_3Adot(theta2).*f_theta3(theta2).*sind(f_theta3dot(theta2)) + f_3A(theta2).*f_theta3dotdot(theta2).*sind(f_theta3(theta2));
