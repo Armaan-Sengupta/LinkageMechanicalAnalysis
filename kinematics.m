a87c25d3af1bf0dcdc55e491c5f932d018e83914
Armaan Sengupta <senguptaarmaan@gmail.com>
2025-02-16 23:25:31 -0500

    Paranethsis and theta dot instead of theta issues fixed

--- kinematics.m
+++ kinematics.m
@@ -23,7 +23,7 @@ f_r4dot = @(theta2) -1*R3.*f_theta3dot(theta2).*sind(f_theta3(theta2)) - R2*thet
 f_theta3dotdot = @(theta2) -1*(R3.*(f_theta3dot(theta2).^2).*sind(f_theta3(theta2)) + R2*theta2_velocity.*sind(theta2)) ./ (R3.*cosd(f_theta3(theta2)));
 
 % Function for length of link 4 acceleration with respect to theta2
-f_r4dotdot = @(theta2) -1*R3.*(f_theta3dotdot(theta2).*sind(f_theta3(theta2)) + (f_theta3(theta2).^2).*cosd(f_theta3(theta2))) + (R2.*(theta2_velocity.^2)).*cosd(theta2);
+f_r4dotdot = @(theta2) -1*R3.*(f_theta3dotdot(theta2).*sind(f_theta3(theta2)) + (f_theta3dot(theta2).^2).*cosd(f_theta3(theta2))) + (R2.*(theta2_velocity.^2)).*cosd(theta2);
 
 % Function for theta6 with respect to theta2
 f_theta6 = @(theta2) -1*asind(R2/R6.*sind(f_theta3(theta2) - theta2)) - f_theta3(theta2);
@@ -46,7 +46,7 @@ f_ky = @(theta2) (-1*R6.*(f_theta6dot(theta2).^2).*sind(f_theta6(theta2)) - 2.*f
 f_kx = @(theta2) -1*R6.*(f_theta6dot(theta2).^2).*cosd(f_theta6(theta2)) + 2.*f_3Adot(theta2).*f_theta3dot(theta2).*sind(f_theta3(theta2)) + f_3A(theta2).*f_theta3dotdot(theta2).*sind(f_theta3(theta2)) + f_3A(theta2).*(f_theta3dot(theta2).^2).*cosd(f_theta3(theta2)) + R2.*(theta2_velocity^2).*cosd(theta2);
 
 % Function for acceleration of link 3A with respect to theta2
-f_3Adotdot = @(theta2) -1.*(f_3A(theta2).*sind(f_theta3(theta2)) - f_ky(theta2)).*sind(f_theta6(theta2)./(cosd(f_theta6(theta2)).*cosd(f_theta3(theta2))) + f_kx(theta2)./cosd(f_theta3(theta2)));
+f_3Adotdot = @(theta2) (- (f_3A(theta2).*sind(f_theta3(theta2)) - f_ky(theta2)).*sind(f_theta6(theta2)) + f_kx(theta2)) ./ (cosd(f_theta3(theta2)).*cosd(f_theta6(theta2)));
 
 % Function for acceleration of theta6 with respect to theta2
 f_theta6dotdot = @(theta2) (f_3Adotdot(theta2).*sind(f_theta3(theta2)) - f_ky(theta2)) ./ (R6.*cosd(f_theta6(theta2)));
